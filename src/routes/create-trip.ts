fa90382f2b04f79a7025176e0ae88cbef38137e3
Bruno Henrique <brunodorea@outlook.com.br>
2024-07-09 20:14:42 -0300

    :sparkles: feat: aula 02

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,14 +2,9 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod'
 import { prisma } from "../lib/prisma";
-import dayjs from 'dayjs'
-import { getMailClient } from "../lib/mai";
+import { getMailClient } from "../lib/mail";
 import nodemailer from 'nodemailer'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-import 'dayjs/locale/pt-br'
-
-dayjs.locale('pt-br')
-dayjs.extend(localizedFormat)
+import { dayjs } from "../lib/dayjs";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -26,13 +21,9 @@ export async function createTrip(app: FastifyInstance) {
   }, async (request) => {
     const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body
 
-    if (dayjs(starts_at).isBefore(new Date())) {
-      throw new Error('Invalid trip start date.')
-    }
+    if (dayjs(starts_at).isBefore(new Date())) { throw new Error('Invalid trip start date.') }
 
-    if (dayjs(ends_at).isBefore(starts_at)) {
-      throw new Error('Invalid trip end date.')
-    }
+    if (dayjs(ends_at).isBefore(starts_at)) { throw new Error('Invalid trip end date.') }
 
 
     const trip = await prisma.trip.create({
@@ -49,9 +40,7 @@ export async function createTrip(app: FastifyInstance) {
                 is_owner: true,
                 is_confirmed: true
               },
-              ...emails_to_invite.map(email => {
-                return { email }
-              })
+              ...emails_to_invite.map(email => { return { email } })
             ]
           }
         }
@@ -61,7 +50,7 @@ export async function createTrip(app: FastifyInstance) {
     const formattedStartDate = dayjs(starts_at).format('LL')
     const formattedEndDate = dayjs(starts_at).format('LL')
 
-    const confirmationLink = `https://localhost:3333/trips/${trip.id}/confirm`
+    const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`
 
     const mail = await getMailClient()
 
