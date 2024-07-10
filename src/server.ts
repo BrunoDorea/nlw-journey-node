a668189d64513750d63daeca68d3e7f86f7006b4
Bruno Henrique <brunodorea@outlook.com.br>
2024-07-10 12:49:09 -0300

    :sparkles: feat: error handler + client error

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 
-import fastify from "fastify";
+import fastify from "fastify"
 import cors from '@fastify/cors'
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { createTrip } from "./routes/create-trip";
-import { confirmTrip } from "./routes/confirm-trip";
-import { confirmParticipants } from "./routes/confirm-participant";
-import { createActivity } from "./routes/create-activity";
-import { getActivities } from "./routes/get-activities";
-import { createLink } from "./routes/create-link";
-import { getLinks } from "./routes/get-links";
-import { getParticipants } from "./routes/get-participants";
-import { createInvite } from "./routes/create-invite";
-import { updateTrip } from "./routes/update-trip";
-import { getTripDetails } from "./routes/get-trip-details";
-import { getParticipant } from "./routes/get-participant";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import { createTrip } from "./routes/create-trip"
+import { confirmTrip } from "./routes/confirm-trip"
+import { confirmParticipants } from "./routes/confirm-participant"
+import { createActivity } from "./routes/create-activity"
+import { getActivities } from "./routes/get-activities"
+import { createLink } from "./routes/create-link"
+import { getLinks } from "./routes/get-links"
+import { getParticipants } from "./routes/get-participants"
+import { createInvite } from "./routes/create-invite"
+import { updateTrip } from "./routes/update-trip"
+import { getTripDetails } from "./routes/get-trip-details"
+import { getParticipant } from "./routes/get-participant"
+import { errorHandler } from "./error-handler"
 
 const app = fastify()
 
@@ -21,8 +22,10 @@ app.register(cors, {
   origin: '*',
 })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.setErrorHandler(errorHandler)
 
 app.register(createTrip)
 app.register(confirmTrip)
