f9a42fadfad6708652a6f567b64d023f259c84fb
Bruno Henrique <brunodorea@outlook.com.br>
2024-07-10 12:28:52 -0300

    :sparkles: feat: get participants + get trip details

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
 import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify()
 
@@ -32,6 +34,8 @@ app.register(getLinks)
 app.register(getParticipants)
 app.register(createInvite)
 app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log(`Servidor iniciado http://localhost:3333`)
